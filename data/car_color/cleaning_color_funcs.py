a1fd84b0fec5ae5daba9723890dd75827a964c7f
Arvin Behbahani <arvinbehbahani562@gmail.com>
2025-11-18 00:45:32 +0330

    cleaning color data completed

---
+++ data/car_color/cleaning_color_funcs.py
@@ -0,0 +1,36 @@
+def fix_half_space(text):
+    if text is None:
+        return text
+    text = str(text)
+
+    text = text.replace("\u200c", " ")
+
+    text = text.replace("\u200b", " ")
+
+    text = text.replace("\u00A0", " ")
+
+    import re
+    text = re.sub(r"\s+", " ", text).strip()
+    return text
+
+
+def merge_same_colors(color):
+    if color == "طلائی":
+        return "طلایی"
+    elif color == "بادمجونی":
+        return "بادمجانی"
+    elif color == "سفید صدفی":
+        return "سفید"
+    elif color == "عدسی":
+        return "نوک مدادی"
+    elif color == "نقرآبی":
+        return "آبی"
+    elif any([color == "گیلاسی",color == "زرشکی",color == "عنابی"]):
+        return "آلبالویی"
+    elif any([color == "امبر بلک",color == "ذغالی",color == "کربن بلک"]):
+        return "مشکی"
+    elif any([color == "شتری",color == "موکا",color == "خاکی"]):
+        return "قهوع ای"
+    
+    return color
+    
---
+++ data/car_color/data_cleaning_color.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from data.car_color.cleaning_color_funcs import fix_half_space, merge_same_colors
+
+
+
+
+df = pd.read_csv("data/all_cars_data.csv")
+df["color"] = df["color"].apply(fix_half_space)
+df["color"] = df["color"].apply(merge_same_colors)
+df = pd.get_dummies(df, columns=["color"])
+
+df.to_csv("data/cleaned_color_data.csv", index=False)
+
+
